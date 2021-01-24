32a4f39206a358a84392bd087dac285a8ceda184
piladzisi <piladzisi@gmail.com>
2021-01-24 20:14:16 +0200

    handle while in use auth

--- MapApp/MapVC.swift
+++ MapApp/MapVC.swift
@@ -14,27 +14,42 @@ class MapVC: UIViewController, CLLocationManagerDelegate {
     @IBOutlet weak var mapView: MKMapView!
     
     let locationManager = CLLocationManager()
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         customizeNavBar()
         locationManager.delegate = self
-        locationManager.requestWhenInUseAuthorization()
-        locationManager.startUpdatingLocation()
-        
+        locationManager.requestWhenInUseAuthorization() 
         mapView.userTrackingMode = .follow
         
     }
-
+    
     private func customizeNavBar() {
         let green = UIColor(hexString: "009051")
         navigationController?.navigationBar.prefersLargeTitles = true
-       // navigationController?.navigationBar.barTintColor  = green // for small titles
+        // navigationController?.navigationBar.barTintColor  = green // for small titles
         navigationController?.view.backgroundColor = green // for large titles
     }
     
     @IBAction func didTapCameraButton(_ sender: UIButton) {
         print("tap camera")
+    }
+    
+    func locationManagerDidChangeAuthorization(_ manager: CLLocationManager) {
+        switch manager.authorizationStatus {
+        case .authorizedWhenInUse:
+            locationManager.startUpdatingLocation()
+        default:
+            print("can't get location")
+        }
+        switch manager.accuracyAuthorization {
+        case .reducedAccuracy:
+            print("approximate location")
+        case .fullAccuracy:
+            print("getting exact location")
+        @unknown default:
+            print("this should not happen!")
+        }
     }
 }
 
